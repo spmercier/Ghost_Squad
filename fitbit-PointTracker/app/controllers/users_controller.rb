cca4668b70df848fdc646a5bd25d7200ffb5592d
Luke Becker <becker34@ksu.edu>
2015-03-05 15:09:43 -0600

    Deleted code in users controller

--- fitbit-PointTracker/app/controllers/users_controller.rb
+++ fitbit-PointTracker/app/controllers/users_controller.rb
@@ -1,17 +1,3 @@
 class UsersController < ApplicationController
-	def index
 
-	end
-	def new
-		@user = User.new
-	end
-	def create
-		
-	end
-	def show
-		@user = User.find(params[:id])
-		#@points = User.points
-		#or something simlar
-		#needs to atleast get total points for now
-	end
 end
