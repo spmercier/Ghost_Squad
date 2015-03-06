eef3c164b1014d1afc89ac36cf1c052bea21ccb9
PatNHut <hutflesspatrickn@gmail.com>
2015-03-05 20:38:00 -0600

    Quick script to pull fitbit user info

--- fitbit-PointTracker/db/schema.rb
+++ fitbit-PointTracker/db/schema.rb
@@ -13,5 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20150303220912) do
 
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
