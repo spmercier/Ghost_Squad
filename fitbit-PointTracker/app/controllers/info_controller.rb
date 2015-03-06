016f40f8aed20bf214d5e885fefaafb0fcd09d96
Luke Becker <becker34@ksu.edu>
2015-03-05 23:04:07 -0600

    Added read ability to info controller

--- fitbit-PointTracker/app/controllers/info_controller.rb
+++ fitbit-PointTracker/app/controllers/info_controller.rb
@@ -1,7 +1,18 @@
 class InfoController < ApplicationController
   def index
-  	'ruby public/getFitInfo.rb'
-  	
+  	@steps = 0
+  	@hasHitSummary = 0
+  	File.open("public/user.txt", "r").each_line do |line|
+	  if line.include? "summary"
+	  	@hasHitSummary = @hasHitSummary + 1
+	  end
+	  if @hasHitSummary == 1
+	  	if line.include? "steps"
+	  		number = line.scan(/[0-9]+/)
+	  		@steps = number[0].to_i
+	  	end
+	  end
 
+	end
   end
 end
