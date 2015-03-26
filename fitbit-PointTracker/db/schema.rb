39dd70e6f96904e209e7b8f0ae2bedc11aa95e2c
Luke Becker <becker34@ksu.edu>
2015-03-26 15:23:46 -0500

    added migration files

--- fitbit-PointTracker/db/schema.rb
+++ fitbit-PointTracker/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150321170149) do
+ActiveRecord::Schema.define(version: 20150321170203) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "provider"
@@ -22,9 +22,7 @@ ActiveRecord::Schema.define(version: 20150321170149) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
