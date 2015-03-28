4b1002980b7a0d415691999477495cd09e062737
PatNHut <hutflesspatrickn@gmail.com>
2015-03-28 15:58:24 -0500

    added get info functions

--- fitbit-PointTracker/app/controllers/sessions_controller.rb
+++ fitbit-PointTracker/app/controllers/sessions_controller.rb
@@ -1,29 +1,42 @@
 class SessionsController < ApplicationController
-  def new
-  end
+ def new
+ end
 
   def create
-  auth = request.env['omniauth.auth']
-  unless @auth = Authorization.find_from_hash(auth)
-    # Create a new user or add an auth to existing user, depending on
-    # whether there is already a user signed in.
-    @auth = Authorization.create_from_hash(auth)
-  end
-  # Log the authorizing user in.
-  self.current_user = @auth.user
+	auth = request.env['omniauth.auth']
+	unless @auth = Authorization.find_from_hash(auth)
+		# Create a new user or add an auth to existing user, depending on
+		# whether there is already a user signed in.
+		@auth = Authorization.create_from_hash(auth)
+	end
+		# Log the authorizing user in.
+		self.current_user = @auth.user
+	end
 
-	@client ||= Fitgem::Client.new(
-		:consumer_key => '17e009f76e454acda410d3dbeb59d047',
-		:consumer_secret => '1db1d74a7ecb4e97aa426fa42126edd1',
-		:token => @auth.token,
-		:secret => @auth.secret,
-		:user_id => @auth.uid,
-		:ssl => true
-	)
-  
-	render :text => @client.activities_on_date('today') 
-end
+ def failure
+ end
 
-  def failure
-  end
+private
+	def connect
+		@client ||= Fitgem::Client.new(
+			:consumer_key => '17e009f76e454acda410d3dbeb59d047',
+			:consumer_secret => '1db1d74a7ecb4e97aa426fa42126edd1',
+			:token => @auth.token,
+			:secret => @auth.secret,
+			:user_id => @auth.uid,
+			:ssl => true
+		)
+		return @client
+	end 
+
+	def get_activities
+		@client = self.connect
+		@activities = client.activities_on_date('today')
+		return activities
+	end
+	def get_userInfo
+		@client = self.connect
+		@userinfo = client.user_info['user']
+		return userinfo
+	end
 end
