b0a3f6fcb1240363d79729c02c231fe2737de368
PatNHut <hutflesspatrickn@gmail.com>
2015-04-02 20:09:08 -0500

    fixed getinfo functions
    I put the Fun in Function

--- fitbit-PointTracker/app/controllers/sessions_controller.rb
+++ fitbit-PointTracker/app/controllers/sessions_controller.rb
@@ -3,20 +3,37 @@ class SessionsController < ApplicationController
  end
 
  def index
+  
+  
   @points = 0
-
  end
 
   def create
   	auth = request.env['omniauth.auth']
-  	unless @auth = Authorization.find_from_hash(auth)
+	unless @auth = Authorization.find_from_hash(auth)
   		# Create a new user or add an auth to existing user, depending on
   		# whether there is already a user signed in.
   		@auth = Authorization.create_from_hash(auth)
   	end
-  		# Log the authorizing user in.
-  		self.current_user = @auth.user
+  	# Log the authorizing user in.
+  	self.current_user = @auth.user
+	@activities = get_activities(@auth)
+	@steps1000 = false
+	@steps5000 = false
+	@steps10000 = false
+	
+	@steps = @activities['steps']
+	
+	if steps>1000
+		steps1000 = true
 	end
+	if steps>5000
+		steps5000 = true
+	end
+	if steps>10000
+		steps10000 = true
+	end
+end
 
  def failure
  end
@@ -29,25 +46,25 @@ class SessionsController < ApplicationController
   end
 
 private
-	def connect
+	def connect(auth)
 		@client ||= Fitgem::Client.new(
 			:consumer_key => '17e009f76e454acda410d3dbeb59d047',
 			:consumer_secret => '1db1d74a7ecb4e97aa426fa42126edd1',
-			:token => @auth.token,
-			:secret => @auth.secret,
-			:user_id => @auth.uid,
+			:token => auth.token,
+			:secret => auth.secret,
+			:user_id => auth.uid,
 			:ssl => true
 		)
 		return @client
 	end 
 
-	def get_activities
-		@client = self.connect
+	def get_activities(auth)
+		@client = self.connect(auth)
 		@activities = client.activities_on_date('today')
 		return activities
 	end
-	def get_userInfo
-		@client = self.connect
+	def get_userInfo(auth)
+		@client = self.connect(auth)
 		@userinfo = client.user_info['user']
 		return userinfo
 	end
